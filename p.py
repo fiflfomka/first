b70971a7ea33d4a674233c6bbf664e5899ed8d15
fiflfomka <fomindmitriyfoma@mail.ru>
2023-07-04 11:59:15 +0300

    number 915 ready but not tested

--- p.py
+++ p.py
@@ -1,3 +1,10 @@
 
-print("LOPL",end='\n*\n')
-print("hello")
+N = int(input())
+l = (input().split())
+for i in range(N):
+	l[i] = int(l[i])
+l = [0,0] + l
+sums = l.copy()
+for i in range(2,N+2):
+	sums[i] = max( l[i-1], l[i-2] ) + l[i]
+print(sums[-1])
